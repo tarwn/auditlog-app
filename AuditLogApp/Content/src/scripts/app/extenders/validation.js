d8ab0b3b1a1d0e0d6db5320f77dfac7a9b261082
Eli Weinstock-Herman <tarwn@tiernok.com>
2018-06-24 14:42:37 -0400

    Improve URL regex [ch745]

--- AuditLogApp/Content/src/scripts/app/extenders/validation.js
+++ AuditLogApp/Content/src/scripts/app/extenders/validation.js
@@ -54,7 +54,7 @@ export default class Validated {
 
     static urlIsValid(newValue, currentValue, config) {
         // https://github.com/jquery-validation/jquery-validation/blob/c1db10a34c0847c28a5bd30e3ee1117e137ca834/src/core.js#L1349
-        const regex = /^(?:(?:(?:https?|mailto):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
+        const regex = /^(http:\/\/|https:\/\/|mailto:[^@]+@)[a-z0-9\u00a1-\uffff-]+(\.[a-z0-9\u00a1-\uffff-]+)*(\.[a-z]{2,5})?(:[0-9]{1,5})?(\/.*)?$/;
 
         return Validated.stringIsValid(newValue, currentValue, config) &&
             ((!config.isRequired && newValue.length === 0) || regex.test(newValue));
