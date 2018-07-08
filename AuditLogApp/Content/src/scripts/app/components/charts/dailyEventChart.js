92f7ea9a7cccd5d3612c40f562d038b96c54257e
Eli Weinstock-Herman <tarwn@tiernok.com>
2018-07-08 16:31:11 -0400

    Complete dashboard chart [ch659]

--- AuditLogApp/Content/src/scripts/app/components/charts/dailyEventChart.js
+++ AuditLogApp/Content/src/scripts/app/components/charts/dailyEventChart.js
@@ -29,12 +29,38 @@ export default {
                 return this.safeData().length;
             });
 
-            this.chart = {};
+            this.chart = {
+                margin: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 20
+                },
+                base: ko.observable({
+                    width: 600,
+                    height: 300
+                }),
+                dimensions: ko.pureComputed(() => {
+                    const { width, height } = this.chart.base();
+                    const { margin } = this.chart;
+                    return {
+                        width: width - margin.left - margin.right,
+                        height: height - margin.top - margin.bottom
+                    };
+                })
+            };
+            window.addEventListener('resize', () => {
+                this._updateChartSize();
+            });
 
             this.subscriptions = [];
             this.subscriptions.push(this.data.subscribe(() => {
                 this._refreshRender();
             }));
+            this.subscriptions.push(this.chart.dimensions.subscribe(() => {
+                this._refreshRender();
+            }));
+
 
             this._initialRender();
         }
@@ -46,22 +72,13 @@ export default {
         }
 
         _initialRender() {
-            const margin = {
-                top: 20,
-                right: 20,
-                bottom: 70,
-                left: 40
-            };
-            const width = 600 - margin.left - margin.right;
-            const height = 300 - margin.top - margin.bottom;
-
-            if (!this._initializeChart(width, height, margin)) {
+            if (!this._initializeChart()) {
                 setTimeout(() => this._initialRender(), 50);
                 return;
             }
             this.chart.isInitialized = true;
 
-            this._render(width, height, margin);
+            this._render();
         }
 
         _refreshRender() {
@@ -69,24 +86,31 @@ export default {
                 return;
             }
 
-            const margin = {
-                top: 20,
-                right: 20,
-                bottom: 70,
-                left: 40
-            };
-            const width = 600 - margin.left - margin.right;
-            const height = 300 - margin.top - margin.bottom;
-
-            this._render(width, height, margin);
+            this._render();
         }
 
-        _initializeChart(width, height, margin) {
+        _updateChartSize() {
+            const chartDiv = document.getElementById(this.id);
+            this.chart.base({
+                width: chartDiv.clientWidth,
+                height: chartDiv.clientHeight
+            });
+        }
+
+        _initializeChart() {
             // not visible
             if (d3.select(`#${this.id}`).empty()) {
                 return false;
             }
 
+            // wire up real size + resize event
+            this._updateChartSize();
+            window.addEventListener('resize', () => this._updateChartSize());
+
+            const { width, height } = this.chart.dimensions();
+            const { margin } = this.chart;
+
+            // start drawing
             this.chart.svg = d3.select(`#${this.id}`)
                 .append('svg')
                 .attr('width', width + margin.left + margin.right)
@@ -96,41 +120,68 @@ export default {
             this.chart.xScale = d3.scaleBand()
                 .range([0, width])
                 .round(true)
-                .paddingInner(.1);
+                .paddingInner(0.1);
 
             // x-axis drawing
             this.chart.xAxis = d3.axisBottom(this.chart.xScale)
                 .ticks(1, 'no data');
             this.chart.svg.append('g')
-                .attr('transform', `translate(0, ${height})`)
-                .attr("class", "x axis")
+                .attr('transform', `translate(${margin.left}, ${height + margin.top})`)
+                .attr('class', 'x ala-chart-axis')
                 .call(this.chart.xAxis);
 
             // y-axis scale
             // TODO: improve range and tick intervals w/ dynamic rounding
             this.chart.yScale = d3.scaleLinear()
-                .range([height, 0]);
+                .range([height, 0])
+                .nice();
+
+            // y-axis chart bg
+            this.chart.yGrid = d3.axisRight(this.chart.yScale);
+            this.chart.svg.append('g')
+                .attr('transform', `translate(${margin.left},${margin.top})`)
+                .attr('class', 'y ala-chart-grid')
+                .call(this.chart.yGrid);
 
             // y-axis drawing
-            this.chart.yAxis = d3.axisLeft(this.chart.yScale)
-                .ticks(0);
+            this.chart.yAxis = d3.axisLeft(this.chart.yScale);
             this.chart.svg.append('g')
-                .attr('transform', 'translate(0,0)')
-                .attr('class', 'y axis')
+                .attr('transform', `translate(${margin.left},${margin.top})`)
+                .attr('class', 'y ala-chart-axis')
                 .call(this.chart.yAxis);
 
             return true;
         }
 
-        _render(width, height, margin) {
-            const { svg, xScale, xAxis, yScale, yAxis } = this.chart;
+        _render() {
+            const {
+                svg,
+                margin,
+                xScale,
+                xAxis,
+                yScale,
+                yAxis,
+                yGrid
+            } = this.chart;
+            const { width, height } = this.chart.dimensions();
 
             // update the scales from data
             xScale.domain(this.data().map(d => d.date.valueOf()));
-            xAxis.tickFormat((d) => new Date(d).getUTCDate());
-            svg.select('.x.axis').transition().duration(300).call(xAxis);
-            yScale.domain([0, this.eventCountMax()]);
-            yAxis.ticks(4);
+            xAxis.tickFormat(d => new Date(d).getUTCDate());
+            svg.select('.x.ala-chart-axis').transition().duration(300).call(xAxis);
+
+            let yMax = this.eventCountMax();
+            yMax += yMax % 4;
+            yScale.domain([0, yMax]);
+
+            yGrid.ticks(3)
+                .tickSizeInner(width)
+                .tickSizeOuter(0)
+                .tickFormat('');
+            svg.select('.y.ala-chart-grid').call(yGrid);
+
+            yAxis.ticks(3);
+            svg.select('.y.ala-chart-axis').call(yAxis);
 
             // bars
             const bars = svg.selectAll('.ala-chart-bar')
@@ -139,10 +190,9 @@ export default {
             bars.enter()
                 .append('rect')
                 .attr('class', 'ala-chart-bar')
-                .style('fill', 'steelblue')
-                .attr('x', d => xScale(d.date.valueOf()))
-                .attr('width', 15)
-                .attr('y', d => yScale(d.count()))
+                .attr('x', d => margin.left + xScale(d.date.valueOf()))
+                .attr('width', xScale.bandwidth)
+                .attr('y', d => margin.top + yScale(d.count()))
                 .attr('height', d => height - yScale(d.count()));
 
             bars.exit()
@@ -164,7 +214,7 @@ export default {
     },
     template: `
         <div class="ala-daily-event-chart-area">
-            <div data-bind="attr: { id: id }"></div>
+            <div data-bind="attr: { id: id }" class="ala-daily-event-chart"></div>
         </div>
     `
 };
